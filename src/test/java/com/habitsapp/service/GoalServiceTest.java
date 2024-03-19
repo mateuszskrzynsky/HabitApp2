8543e743398443a53105f037bae9ed174f722efb
mateuszskrzynsky <mateusz.skrzynsky@outlook.com>
2024-03-19 18:23:06 +0100

    4.Tests for service package added and checked

--- src/test/java/com/habitsapp/service/GoalServiceTest.java
+++ src/test/java/com/habitsapp/service/GoalServiceTest.java
@@ -1,28 +1,129 @@
 package com.habitsapp.service;
 
+import com.habitsapp.model.Category;
+import com.habitsapp.model.Goal;
+import com.habitsapp.repository.GoalRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class GoalServiceTest {
+
+    @Mock
+    private GoalRepository goalRepository;
+
+    @InjectMocks
+    private GoalService goalService;
+
+//    private Goal goal;
+//
+//    @BeforeEach
+//    public void setUpGoal(){
+//        goal = new Goal();
+//        goal.setId(1L);
+//        goal.setName("Learn programming");
+//        goal.setCategory(Category.EDUCATION);
+//        goal.setStartDate(LocalDate.now());
+//        goal.setEndDate(LocalDate.now().plusMonths(1));
+//    }
 
     @Test
     void findAllGoals() {
+
+        //GIVEN
+        Goal goal1 = new Goal();
+        Goal goal2 = new Goal();
+
+        //WHEN
+        when(goalRepository.findAll()).thenReturn(Arrays.asList(goal1, goal2));
+        List<Goal> goals = goalService.findAllGoals();
+
+        //THEN
+        assertNotNull(goals);
+        assertEquals(2, goals.size());
     }
 
     @Test
     void findGoalById() {
+
+        //GIVEN
+        Goal goal = new Goal();
+        goal.setId(1L);
+
+        //WHEN
+        when(goalRepository.findById(1L)).thenReturn(Optional.of(goal));
+        Optional<Goal> foundGoal = goalService.findGoalById(1L);
+
+        //THEN
+        assertTrue(foundGoal.isPresent());
+        assertEquals(1L, foundGoal.get().getId());
     }
 
     @Test
     void createGoal() {
+
+        //GIVEN
+        Goal goal = new Goal();
+        goal.setName("CEL");
+
+        //WHEN
+        when(goalRepository.save(any(Goal.class))).thenReturn(goal);
+        Goal result = goalService.createGoal(goal);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals("CEL",result.getName());
     }
 
     @Test
     void deleteGoal() {
+
+        //GIVEN
+        Goal goal = new Goal();
+        goal.setId(1L);
+
+        //WHEN
+        when(goalRepository.findById(1L)).thenReturn(Optional.of(goal));
+        doNothing().when(goalRepository).deleteById(1L);
+        boolean isDeleted = goalService.deleteGoal(1L);
+
+        //THEN
+        assertTrue(isDeleted);
+        verify(goalRepository, times(1)).deleteById(1L);
     }
 
     @Test
     void updateGoal() {
+
+        //GIVEN
+        Goal goal = new Goal();
+        goal.setId(1L);
+        goal.setName("ZAROBIĆ PIEWSZY MILION");
+
+        Goal updateGoal = new Goal();
+        updateGoal.setName("PRZEZYC DO NASTEPNEJ WYPŁATY");
+        updateGoal.setCategory(Category.FINANCE);
+
+        //WHEN
+        when(goalRepository.findById(1L)).thenReturn(Optional.of(goal));
+        when(goalRepository.save(any(Goal.class))).thenAnswer(c -> c.getArgument(0));
+        Goal newGoal = goalService.updateGoal(1L, updateGoal);
+
+        //THEN
+        assertNotNull(newGoal);
+        assertEquals("PRZEZYC DO NASTEPNEJ WYPŁATY", newGoal.getName());
+        assertEquals(Category.FINANCE, newGoal.getCategory());
     }
 }
--- src/test/java/com/habitsapp/service/HabitServiceTest.java
+++ src/test/java/com/habitsapp/service/HabitServiceTest.java
@@ -1,24 +1,94 @@
 package com.habitsapp.service;
 
+import com.habitsapp.model.Goal;
+import com.habitsapp.model.Habit;
+import com.habitsapp.repository.GoalRepository;
+import com.habitsapp.repository.HabitRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
+@ExtendWith(MockitoExtension.class)
 class HabitServiceTest {
+    @Mock
+    private HabitRepository habitRepository;
+
+    @InjectMocks
+    private HabitService habitService;
+
 
     @Test
     void findAllHabits() {
+        //GIVEN
+        Habit habit1 = new Habit();
+        Habit habit2 = new Habit();
+
+        //WHEN
+        when(habitRepository.findAll()).thenReturn(Arrays.asList(habit1, habit2));
+        List<Habit> habits = habitService.findAllHabits();
+
+        //THEN
+        assertNotNull(habits);
+        assertEquals(2, habits.size());
     }
 
     @Test
     void findHabitById() {
+
+        //GIVEN
+        Habit habit = new Habit();
+        habit.setId(1L);
+
+        //WHEN
+        when(habitRepository.findById(1L)).thenReturn(Optional.of(habit));
+        Optional<Habit> foundHabit = habitService.findHabitById(1L);
+
+        //THEN
+        assertTrue(foundHabit.isPresent());
+        assertEquals(1L, foundHabit.get().getId());
     }
 
     @Test
     void createHabit() {
+
+        //GIVEN
+        Habit habit = new Habit();
+        habit.setName("NAWYK");
+
+        //WHEN
+        when(habitRepository.save(any(Habit.class))).thenReturn(habit);
+        Habit result = habitService.createHabit(habit);
+
+        //THEN
+        assertNotNull(result);
+        assertEquals("NAWYK",result.getName());
     }
 
     @Test
     void deleteHabit() {
+
+        //GIVEN
+        Habit habit = new Habit();
+        habit.setId(1L);
+
+        //WHEN
+        when(habitRepository.findById(1L)).thenReturn(Optional.of(habit));
+        doNothing().when(habitRepository).deleteById(1L);
+        boolean isDeleted = habitService.deleteHabit(1L);
+
+        //THEN
+        assertTrue(isDeleted);
+        verify(habitRepository, times(1)).deleteById(1L);
     }
 }
